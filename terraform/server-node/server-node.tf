de582443df50acc0c4aaa0222ba1e68daeefc848
Marcus Yanello <myanello@rescale.com>
2025-01-20 11:06:31 -0600

    change secrets around

--- terraform/server-node/server-node.tf
+++ terraform/server-node/server-node.tf
@@ -3,12 +3,13 @@ provider "kubernetes" {
   config_context = "picontext"
 }
 
-resource "kubernetes_node_taint" "gpu" {
+resource "kubernetes_node_taint" "no_schedule" {
   metadata {
     name = "marcus-server"
   }
+
   taint {
-    key    = "nvidia.com/gpu" # Your taint key
+    key    = "node-role.kubernetes.io/server" # Your taint key
     value  = "true"                           # Your taint value
     effect = "NoSchedule"                     # NoSchedule, PreferNoSchedule, or NoExecute
   }
