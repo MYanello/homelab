6219db1e50af3a65e4a93df384566a181d918442
Marcus Yanello <myanello@rescale.com>
2025-01-22 08:51:20 -0600

    gpu taint

--- terraform/server-node/server-node.tf
+++ terraform/server-node/server-node.tf
@@ -7,9 +7,8 @@ resource "kubernetes_node_taint" "no_schedule" {
   metadata {
     name = "marcus-server"
   }
-
   taint {
-    key    = "node-role.kubernetes.io/server" # Your taint key
+    key    = "nvidia.com/gpu" # Your taint key
     value  = "true"                           # Your taint value
     effect = "NoSchedule"                     # NoSchedule, PreferNoSchedule, or NoExecute
   }
