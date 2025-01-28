b95b542500dcc3eedb7e7a52bf3582dbe427b725
Marcus Yanello <myanello@rescale.com>
2025-01-27 21:38:58 -0600

    node labels

---
+++ terraform/nodes/main.tf
@@ -0,0 +1,38 @@
+provider "kubernetes" {
+  config_path    = "~/.kube/config"
+  config_context = "picontext"
+}
+
+# resource "kubernetes_node_taint" "no_schedule" {
+#   metadata {
+#     name = "marcus-server"
+#   }
+#   taint {
+#     key    = "nvidia.com/gpu" # Your taint key
+#     value  = "true"                           # Your taint value
+#     effect = "NoSchedule"                     # NoSchedule, PreferNoSchedule, or NoExecute
+#   }
+# }
+
+# resource "kubernetes_labels" "server" {
+#   api_version = "v1"
+#   kind        = "Node"
+#   metadata {
+#     name = "marcus-server"
+#   }
+#   labels = {
+#     "nvidia.com/gpu.present" = "true",
+#     "accelerator" = "coral-tpu"
+#   }
+# }
+
+resource "kubernetes_labels" "hp-worker" {
+    api_version = "v1"
+    kind        = "Node"
+    metadata {
+      name = "hp-worker"
+    }
+  labels = {
+    "storage" = "ssd"
+  }
+}
