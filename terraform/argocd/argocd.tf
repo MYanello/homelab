0c849b39aedbb1884a13d5bca675fc32a617c2f7
Marcus <marcus@yanello.net>
2025-02-11 21:41:40 -0600

    clean up argo tf

--- terraform/argocd/argocd.tf
+++ terraform/argocd/argocd.tf
@@ -1,5 +1,5 @@
 provider "kubernetes" {
-  config_path = "~/.kube/config"
+  config_path    = "~/.kube/config"
   config_context = "picontext"
 }
 
@@ -8,17 +8,21 @@ resource "kubernetes_manifest" "argocd_appset" {
     "apiVersion" = "argoproj.io/v1alpha1"
     "kind"       = "ApplicationSet"
     "metadata" = {
-      "name" = "argocd-apps"
+      "name"      = "argocd-apps"
       "namespace" = "argocd"
     }
     "spec" = {
       "generators" = [{
         "git" = {
-          "repoURL"   = "https://github.com/myanello/homelab"
-          "revision"  = "main"
+          "repoURL"  = "https://github.com/myanello/homelab"
+          "revision" = "main"
           "directories" = [
             {
               "path" = "argocd/apps/**"
+            },
+            {
+              "path"    = "argocd/apps/dev",
+              "exclude" = true
             }
           ]
         }
@@ -36,7 +40,7 @@ resource "kubernetes_manifest" "argocd_appset" {
             "path"           = "{{path}}"
           }
           "destination" = {
-            "name" = "in-cluster"
+            "name"      = "in-cluster"
             "namespace" = "{{path.basename}}"
           }
           "syncPolicy" = {
@@ -54,46 +58,3 @@ resource "kubernetes_manifest" "argocd_appset" {
   }
 }
 
-resource "kubernetes_manifest" "argocd_cm" {
-  manifest = {
-    apiVersion = "v1"
-    kind       = "ConfigMap"
-    metadata = {
-      name      = "argocd-cm"
-      namespace = "argocd"
-      labels = {
-        "app.kubernetes.io/part-of" = "argocd"
-      }
-    }
-    data = {
-      "kustomize.buildOptions" = "--enable-helm --enable-alpha-plugins --enable-exec"
-    }
-  }
-}
-
-# resource "kubernetes_ingress_v1" "argocd_ingress" {
-#   metadata {
-#     name      = "argocd-ingress"
-#     namespace = "argocd"
-#   }
-#   spec {
-#     ingress_class_name = "istio"
-#     rule {
-#       host = "argocd.yanello.net"
-#       http {
-#         path {
-#           path      = "/"
-#           path_type = "Prefix"
-#           backend {
-#             service {
-#               name = "argocd-server"
-#               port {
-#                 number = 80
-#               }
-#             }
-#           }
-#         }
-#       }
-#     }
-#   }
-# }
