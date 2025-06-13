762b60e32923b3b60dc46617086f93c0996c16ae
Marcus <marcus@yanello.net>
2025-06-13 15:02:06 -0500

    use argocd for talos cluster

--- terraform/argocd/argocd.tf
+++ terraform/argocd/argocd.tf
@@ -1,6 +1,6 @@
 provider "kubernetes" {
   config_path    = "~/.kube/config"
-  config_context = "picontext"
+  config_context = "homecontext"
 }
 
 resource "kubernetes_manifest" "argocd_appset" {
@@ -56,6 +56,57 @@ resource "kubernetes_manifest" "argocd_appset" {
                 "istio-injection" = "disabled"
               }
             }
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_manifest" "argocd_talos_appset" {
+  manifest = {
+    "apiVersion" = "argoproj.io/v1alpha1"
+    "kind"       = "ApplicationSet"
+    "metadata" = {
+      "name"      = "argocd-talos-apps"
+      "namespace" = "argocd"
+    }
+    "spec" = {
+      "generators" = [{
+        "git" = {
+          "repoURL"  = "https://github.com/myanello/homelab"
+          "revision" = "main"
+          "directories" = [
+            {
+              "path" = "talos/**"
+            }
+          ]
+        }
+      }]
+      "template" = {
+        "metadata" = {
+          "name"      = "{{path.basename}}-talos-app"
+          "namespace" = "argocd"
+        }
+        "spec" = {
+          "project" = "default"
+          "source" = {
+            "repoURL"        = "https://github.com/myanello/homelab"
+            "targetRevision" = "main"
+            "path"           = "{{path}}"
+          }
+          "destination" = {
+            "name"      = "talos"
+            "namespace" = "{{path.basename}}"
+          }
+          "syncPolicy" = {
+            "automated" = {
+              "prune"    = true
+              "selfHeal" = true
+            }
+            "syncOptions" = [
+              "CreateNamespace=true"
+            ]
           }
         }
       }
