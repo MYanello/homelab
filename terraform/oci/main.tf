ab539adb33b3c4b1e63f9205c248eb7193610dfc
Marcus <marcus@yanello.net>
2025-06-26 07:47:23 -0500

    reorganize tf

--- terraform/oci/main.tf
+++ terraform/oci/main.tf
@@ -1,19 +1,6 @@
-provider "oci" {
-  tenancy_ocid     = data.sops_file.this.data["tenancy_ocid"]
-  user_ocid        = data.sops_file.this.data["user_ocid"]
-  fingerprint      = data.sops_file.this.data["fingerprint"]
-  private_key_path = data.sops_file.this.data["private_key_path"]
-  region           = data.sops_file.this.data["region"]
-}
-
 locals {
   compartment_id   = var.compartment_id
   key_display_name = "hc-key"
-}
-
-data "sops_file" "this" {
-  source_file     = "provider.enc.yaml"
-  input_type = "yaml"
 }
 
 resource "oci_kms_vault" "hcl_vault" {
@@ -32,17 +19,4 @@ resource "oci_kms_key" "this" {
   }
   management_endpoint = oci_kms_vault.hcl_vault.management_endpoint
   protection_mode     = "HSM"
-}
-
-terraform {
-  required_providers {
-    oci = {
-      source  = "oracle/oci"
-      version = ">= 4.0"
-    }
-    sops = {
-      source  = "carlpett/sops"
-      version = "~> 1.0"
-    }
-  }
 }
