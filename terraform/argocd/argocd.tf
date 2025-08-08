51e0edfe60664bb814372cbf65e0e480aa55629a
Marcus <marcus@yanello.net>
2025-08-08 12:25:01 -0500

    use local tf backend

--- terraform/argocd/argocd.tf
+++ terraform/argocd/argocd.tf
@@ -3,21 +3,21 @@ provider "kubernetes" {
   config_context = "homecontext"
 }
 
-terraform {
-  backend "s3" {
-    bucket = "terraform"
-    endpoints = {
-      s3 = "https://minio.yanello.net"
-    }
-    key                         = "argocd"
-    region                      = "homelab"
-    skip_credentials_validation = true # Skip AWS related checks and validations
-    skip_requesting_account_id  = true
-    skip_metadata_api_check     = true
-    skip_region_validation      = true
-    use_path_style              = true # Enable path-style S3 URLs, required for minio (https://<HOST>/<BUCKET> https://developer.hashicorp.com/terraform/language/settings/backends/s3#use_path_style
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket = "terraform"
+#     endpoints = {
+#       s3 = "https://minio.yanello.net"
+#     }
+#     key                         = "argocd"
+#     region                      = "homelab"
+#     skip_credentials_validation = true # Skip AWS related checks and validations
+#     skip_requesting_account_id  = true
+#     skip_metadata_api_check     = true
+#     skip_region_validation      = true
+#     use_path_style              = true # Enable path-style S3 URLs, required for minio (https://<HOST>/<BUCKET> https://developer.hashicorp.com/terraform/language/settings/backends/s3#use_path_style
+#   }
+# }
 
 resource "kubernetes_manifest" "argocd_appset" {
   manifest = {
--- terraform/vault/providers.tf
+++ terraform/vault/providers.tf
@@ -9,19 +9,19 @@ terraform {
       version = "~> 1.2.0"
     }
   }
-  backend "s3" {
-    bucket = "terraform"
-    endpoints = {
-      s3 = "https://minio.yanello.net"
-    }
-    key                         = "vault"
-    region                      = "homelab"
-    skip_credentials_validation = true # Skip AWS related checks and validations
-    skip_requesting_account_id  = true
-    skip_metadata_api_check     = true
-    skip_region_validation      = true
-    use_path_style              = true # Enable path-style S3 URLs, required for minio (https://<HOST>/<BUCKET> https://developer.hashicorp.com/terraform/language/settings/backends/s3#use_path_style
-  }
+  # backend "s3" {
+  #   bucket = "terraform"
+  #   endpoints = {
+  #     s3 = "https://minio.yanello.net"
+  #   }
+  #   key                         = "vault"
+  #   region                      = "homelab"
+  #   skip_credentials_validation = true # Skip AWS related checks and validations
+  #   skip_requesting_account_id  = true
+  #   skip_metadata_api_check     = true
+  #   skip_region_validation      = true
+  #   use_path_style              = true # Enable path-style S3 URLs, required for minio (https://<HOST>/<BUCKET> https://developer.hashicorp.com/terraform/language/settings/backends/s3#use_path_style
+  # }
 }
 
 provider "vault" {
