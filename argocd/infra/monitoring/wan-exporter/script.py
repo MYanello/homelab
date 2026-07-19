f85450dbcbc0b994e3f2cc6381b4104a80e8e8ec
Marcus <marcus@yanello.net>
2026-07-19 11:16:07 -0400

    add wan exporter

---
+++ argocd/infra/monitoring/wan-exporter/script.py
@@ -0,0 +1,40 @@
+import asyncio
+import time
+
+import aiohttp
+from aiohttp.client import ClientTimeout
+from prometheus_client import Gauge, Info, start_http_server
+
+wan_ip = Info("wan_ip", "Current public IP")
+last_ip = Gauge("wan_last_scrape", "Timestamp of last successful scrape")
+last_change = Gauge("wan_ip_last_change", "Timestamp of last IP change")
+
+
+async def update():
+    async with aiohttp.ClientSession() as session:
+        prev_ip = ""
+        while True:
+            try:
+                async with session.get(
+                    "https://ifconfig.co",
+                    timeout=ClientTimeout(5),
+                    headers={"Accept": "application/json"},
+                ) as resp:
+                    data = await resp.json()
+                    ip = data.get("ip", "unknown")
+                    wan_ip.info({"ip": ip})
+                    last_ip.set(time.time())
+                    if ip != prev_ip:
+                        last_change.set(time.time())
+                        print(f"Got new IP: {ip}")
+                        prev_ip = ip
+            except Exception as e:
+                print(f"Error getting IP: {e}")
+                pass
+            await asyncio.sleep(120)
+
+
+if __name__ == "__main__":
+    print("Starting WAN IP exporter service")
+    start_http_server(9101)
+    asyncio.run(update())
